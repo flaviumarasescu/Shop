03b0ce9cf95eca00a32721ce938c9471c707723d
flaviumarasescu <50264537+flaviumarasescu@users.noreply.github.com>
2021-03-13 13:03:42 +0200

    Update signup.inc.php

--- Shop/includes/signup.inc.php
+++ Shop/includes/signup.inc.php
@@ -28,26 +28,23 @@ if(isset($_POST['signup-submit'])){ //verfica daca user ul a ajuns la pagina pri
         exit();
     }
     else{
-        $sql = "SELECT uidUsers FROM users WHERE uidUsers=?"; //uidUsers=? ca userul sa nu poata scrie cod sql in formular
+        $sql = "SELECT uidUsers FROM users WHERE uidUsers=?";
         $stmt = mysqli_stmt_init($conn);
         if(!mysqli_stmt_prepare($stmt, $sql)){
             header("Location: ../signup.php?error=sqlerror");
         exit();
         }
         else{
-                //preia info de la user si le pun in bd
-            mysqli_stmt_bind_param( $stmt, "s", $username ); //s - string - data type
+            mysqli_stmt_bind_param( $stmt, "s", $username );
             mysqli_stmt_execute($stmt);
             //pentru ca un user sa nu poata folosi o adresa care a mai fost introdusa
             mysqli_stmt_store_result($stmt);
-            //cand primim info de la db le primim ca linii si rezultatul ar trebui sa fie 0 sau 1
             $resultCheck = mysqli_stmt_num_rows($stmt);
             if($resultCheck > 0 ){
                 header("Location: ../signup.php?error=usertaken&mail=".$email);
                 exit();
             }
             else{
-                //Populare tabel 
                 $sql = "INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES (?, ?, ?)";
                 $stmt = mysqli_stmt_init($conn);
                 if(!mysqli_stmt_prepare($stmt, $sql)){
@@ -55,7 +52,6 @@ if(isset($_POST['signup-submit'])){ //verfica daca user ul a ajuns la pagina pri
                 exit();
                 }
                 else{
-                    //metoda de hash ing pentru parola pentru a o cripta
             $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
                         
             mysqli_stmt_bind_param( $stmt, "sss", $username, $email, $hashedPwd); 
@@ -67,12 +63,12 @@ if(isset($_POST['signup-submit'])){ //verfica daca user ul a ajuns la pagina pri
             }
         }
     }
-    //inchidem legaturile pentru a salva spatiu
+
 mysqli_stmt_close($stmt);
 mysqli_close($conn);
 
 }
-else{ //daca nu a ajuns aici prin butonul de submit atunci il trimitem inapoi
+else{
     header("Location: ../signup.php");
                 exit();
 }
